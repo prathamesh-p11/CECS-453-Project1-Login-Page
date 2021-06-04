d64f2a1c5d745c39ecbdc956908ffae7a79ab079
Prathamesh Patil <prathameshp2472@gmail.com>
2021-06-03 22:00:25 -0700

    Login page initial design for mobile and tab

---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,27 @@
+/*
+    Assignment1
+    Written by:
+        Arjang Fahim 098765432
+        Arjang Fahim 123455678
+
+
+ */
+
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
